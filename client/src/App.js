70d2828fb9012db0801704d6a8438727013eb71d
rawand-hawwari <rawandh3@gmail.com>
2023-10-31 17:12:08 +0300

    login and signup

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Home from './pages/Home';
+// import Portal from './components/Portal';
+import Login from './pages/Login';
+import Signup from './pages/Signup';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router >
+        <Header />
+        <div className='h-screen' id='root'>
+
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/login' element={<Login />} />
+            <Route path='/signup' element={<Signup />} />
+          </Routes>
+        </div>
+        <Footer />
+      </Router>
     </div>
   );
 }
---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Signup from './Signup'
+
+const Home = () => {
+  return (
+    <div id='Home'>
+        
+    </div>
+  )
+}
+
+export default Home
