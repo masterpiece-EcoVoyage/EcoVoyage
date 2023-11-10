a5595808c508cf7564c6ca8a394a4b9aca5be094
rawand-hawwari <rawandh3@gmail.com>
2023-11-10 20:29:25 +0300

    update

--- client/src/App.js
+++ client/src/App.js
@@ -2,20 +2,21 @@ import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Header from './components/Header';
 import Footer from './components/Footer';
-import Home from './pages/Home';
-// import Portal from './components/Portal';
-import Login from './pages/Login';
-import Signup from './pages/Signup';
+import Home from './components/Website/Home';
+import Login from './components/Users/Login';
+import Signup from './components/Users/Signup';
+import Destinations from './components/Website/Destinations';
 
 function App() {
   return (
     <div className="App">
       <Router >
         <Header />
-        <div className='h-screen' id='root'>
+        <div className='h-full' id='root'>
 
           <Routes>
             <Route path='/' element={<Home />} />
+            <Route path='/destinations' element={<Destinations />} />
             <Route path='/login' element={<Login />} />
             <Route path='/signup' element={<Signup />} />
           </Routes>
